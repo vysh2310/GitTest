fc94ae751c871e8347cc23b47191ec31c173a2f0
vysh2310 <48465529+vysh2310@users.noreply.github.com>
2020-03-20 13:06:08 +0530

    Add files via upload

---
+++ example.c
@@ -0,0 +1,66 @@
+/**
+  * Polyspace example.
+  *       Copyright 2012-2013 The MathWorks, Inc.
+  */
+
+#include <assert.h>
+#include <math.h>
+#include "include.h"
+
+
+/* Internal functions        */
+/* Needed for MISRA-rule 8.1 */
+static int get_oil_pressure(void);
+void Pointer_Arithmetic(void);
+extern int get_bus_status(void);
+
+void unreachable_code(void)
+{
+    int i;
+    
+	i = get_bus_status();
+}
+
+static int get_oil_pressure(void)
+{
+    int i;
+    
+	i = get_bus_status();
+	
+	if (i < 1)
+	{
+		i = 1;
+	}
+	    
+    return i;
+}
+
+/* Here we demonstrate Polyspace Verifier's ability to track a
+   pointer across numerous pointer arithmetic operations,
+   including invalid addresses and back to valid addresses.
+*/
+void Pointer_Arithmetic(void)
+{
+    int array[100];
+    int i, *p = array;
+
+    for (i = 0; i < 100; i++) {
+        *p = 0;
+        p++;
+    }
+
+    if (get_bus_status() > 0) {
+        if (get_oil_pressure() > 0) {
+            *p = 5; /* Out of bounds */
+        } else {
+            i++;
+            unreachable_code();
+        }
+    }
+	
+    i = get_bus_status();
+	
+    if (i >= 0) {
+        *(p - i) = 10;
+    }
+}
---
+++ include.h
@@ -0,0 +1,36 @@
+/**
+  * Polyspace example.
+  * This file defines the high-level procedures for the tasking examples.
+  *       Copyright 2012-2013 The MathWorks, Inc.
+  */
+
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#define true 1
+#define false 0
+
+#define checksum 1
+#define new_move 0
+#define previous_move 1
+
+#define MAX_SIZE 10
+
+
+/* automatically stubbed functions */
+extern void SEND_MESSAGE(int status, const char* message);
+extern int read_bus_status(void);
+extern int error_on_bus(void);
+extern int read_on_bus(void);
+
+/* internal functions */
+int    random_int(void);
+float  random_float(void);
+extern void Pointer_Arithmetic(void);
+extern int  tab[MAX_SIZE];
+extern int  PowerLevel;
+extern int  first_paiload;
+extern int  second_paiload;
+extern int* arr;
+
+#endif
